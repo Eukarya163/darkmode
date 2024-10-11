070ff076c730d3378fa097303df4e3b622e00c02
Eukarya163 <Eukarya163@users.noreply.github.com>
2024-10-11 18:47:37 +0900

    完成

--- script.js
+++ script.js
@@ -2,4 +2,12 @@ const btn = document.querySelector('#btn');
 
 btn.addEventListener('click', () => {
   document.body.classList.toggle('dark-theme');
+
+  if(btn1.textContent === 'ダーク') {
+    btn1.textContent = 'ライト';
+  } else {
+    btn1.textContent = 'ダーク';
+  }
+
+
 });
